440b5c87cb5dc414827c8e44a21d340de0b3204a
William Hartman <hartman.william@gmail.com>
2018-10-05 16:28:00 -0700

    its the weekend!

---
+++ src/components/Login/Login.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import './Login.css';
+import { getUserData } from './../../redux/reducer';
+import { withRouter } from "react-router";
+import { connect } from "react-redux";
+
+
+class Login extends Component {
+    constructor(props){
+        super(props)
+
+        this.state = {
+
+        }
+    }
+
+    componentDidMount = () => {
+        if(this.props.user.user_id){
+            this.props.history.push('/dashboard');
+        }
+    }
+
+    render (){
+        return (
+            <div className="Login">
+                Login
+            </div>
+        )
+    }
+}
+
+function mapStateToProps(state){
+    return {
+        user: state.reducer.user
+    }
+  }
+  
+export default withRouter(connect(mapStateToProps, { getUserData })(Login));
